90bf3f8f164e74cfaea6ad46117b2bd9eef1dc45
Grenish rai <mrcoder2033d@gmail.com>
2025-10-14 21:49:27 +0530

    Wrap MDX pre with CodeBlock and Pre

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,18 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Mermaid } from "./components/mdx/mermaid";
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     Mermaid,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components,
   };
 }
